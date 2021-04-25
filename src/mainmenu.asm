3683d986a078caaf7401258d63bed96927b1056f
InsaneFirebat <InsaneFirebat@gmail.com>
2021-04-24 22:00:49 -0400

    Option to Clear Controller Shortcuts
    Set Menu shortcut to default, zero the rest

--- src/mainmenu.asm
+++ src/mainmenu.asm
@@ -978,6 +978,7 @@ CtrlMenu:
     dw #ctrl_full_equipment
     dw #ctrl_kill_enemies
     dw #ctrl_reveal_damage
+    dw #ctrl_clear_shortcuts
     dw #$0000
     %cm_header("CONTROLLER SHORTCUTS")
 
@@ -1005,3 +1006,21 @@ ctrl_kill_enemies:
 
 ctrl_reveal_damage:
     %cm_ctrl_shortcut("Toggle Boss Dmg", !sram_ctrl_reveal_damage)
+
+ctrl_clear_shortcuts:
+    %cm_jsr("Clear Shortcuts", action_clear_shortcuts, #$0000)
+
+action_clear_shortcuts:
+{
+    TYA
+    STA !sram_ctrl_save_state
+    STA !sram_ctrl_load_state
+    STA !sram_ctrl_load_last_preset
+    STA !sram_ctrl_full_equipment
+    STA !sram_ctrl_kill_enemies
+    STA !sram_ctrl_reset_segment_timer
+    STA !sram_ctrl_reveal_damage
+    ; default for menu, Start + Select
+    LDA #$3000 : STA !sram_ctrl_menu
+    RTS
+}
