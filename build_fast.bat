af7171580b714e9ea150560c108d4640c6103e1d
InsaneFirebat <InsaneFirebat@gmail.com>
2021-07-05 00:32:09 -0400

    Room Timers with Minimap

--- build_fast.bat
+++ build_fast.bat
@@ -4,6 +4,6 @@ echo Building SM Practice Hack
 
 cd build
 echo Building and pre-patching saveless extras version
-cp sm_orig.sfc FastBuildTest.sfc && cd ..\src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 main.asm ..\build\FastBuildTest.sfc && cd ..
+cp sm_orig.sfc FastBuildTest.sfc && cd ..\src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 main.asm ..\build\FastBuildTest.sfc && cd ..
 
 PAUSE
