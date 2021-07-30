bfc7871d2f088cedf1cb7a727cff142d96d9b183
InsaneFirebat <InsaneFirebat@gmail.com>
2021-07-30 11:09:36 -0400

    Optimizing for extra game loop features
    Skip to handling inputs if !ram_game_loop_extras = 0
    Set above flag to 1 when enabling game loop features in the menu
    Check if the flag should be reset when exiting the menu
    Move Infinite Ammo back to normal builds

--- src/mainmenu.asm
+++ src/mainmenu.asm
@@ -819,9 +819,7 @@ MiscMenu:
     dw #misc_music_toggle
     dw #misc_transparent
     dw #misc_invincibility
-    if !FEATURE_EXTRAS
-        dw #misc_infiniteammo
-    endif
+    dw #misc_infiniteammo
     dw #misc_magnetstairs
     dw #$0000
     %cm_header("MISC OPTIONS")
@@ -839,10 +837,10 @@ misc_babyslowdown:
     %cm_toggle("Baby Slowdown", $7E0A66, #$0002, #0)
 
 misc_magicpants:
-    %cm_toggle_bit("Magic Pants", !ram_magic_pants_enabled, #$0001, #0)
+    %cm_toggle_bit("Magic Pants", !ram_magic_pants_enabled, #$0001, GameLoopExtras)
 
 misc_spacepants:
-    %cm_toggle_bit("Space Pants", !ram_magic_pants_enabled, #$0002, #0)
+    %cm_toggle_bit("Space Pants", !ram_magic_pants_enabled, #$0002, GameLoopExtras)
 
 misc_fanfare_toggle:
     %cm_toggle("Fanfare", !sram_fanfare_toggle, #$0001, #0)
@@ -877,10 +875,8 @@ misc_transparent:
 misc_invincibility:
     %cm_toggle_bit("Invincibility", $7E0DE0, #$0007, #0)
 
-if !FEATURE_EXTRAS
-    misc_infiniteammo:
-        %cm_toggle_bit("Infinite Ammo", !ram_infinite_ammo, #$0001, #0)
-endif
+misc_infiniteammo:
+    %cm_toggle_bit("Infinite Ammo", !ram_infinite_ammo, #$0001, #GameLoopExtras)
 
 misc_magnetstairs:
     %cm_toggle("Magnet Stairs Fix", !ram_magnetstairs, #$0001, #.routine)
@@ -1812,7 +1808,7 @@ game_clear_minimap:
     RTS
 
 game_metronome:
-    %cm_toggle("Metronome", !ram_metronome, #$0001, #0)
+    %cm_toggle("Metronome", !ram_metronome, #$0001, GameLoopExtras)
 
 game_metronome_tickrate:
     %cm_numfield("Metronome Tickrate", !sram_metronome_tickrate, 1, 255, 1, #.routine)
@@ -1831,6 +1827,18 @@ game_metronome_sfx:
         db #$28, " POWER BEAM", #$FF
         db #$28, "     SPAZER", #$FF
     db #$FF
+
+GameLoopExtras:
+{
+    LDA !ram_magic_pants_enabled : BNE .enabled
+    LDA !ram_metronome : BNE .enabled
+    LDA !ram_infinite_ammo : BNE .enabled
+    LDA #$0000 : STA !ram_game_loop_extras
+    RTS
+  .enabled
+    LDA #$0001 : STA !ram_game_loop_extras
+    RTS
+}
 
 
 ; ----------
