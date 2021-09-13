babfd6acb9c1aa288c4ce6aa96997d1f08f070e1
InsaneFirebat <InsaneFirebat@gmail.com>
2021-09-13 08:04:02 -0400

    Hide Grapple/Xray Icons
    Importing idlechild's work
    
    Co-Authored-By: idlechild <68176797+idlechild@users.noreply.github.com>

--- src/mainmenu.asm
+++ src/mainmenu.asm
@@ -178,7 +178,7 @@ MainMenu:
     dw #mm_goto_ctrlsmenu
     dw #mm_goto_IFBmenu
     dw #$0000
-    %cm_header("CUSTOM INFOHUD V2.2.7.2")
+    %cm_header("CUSTOM INFOHUD V2.2.7.3")
 
 mm_goto_equipment:
     %cm_submenu("Equipment", #EquipmentMenu)
