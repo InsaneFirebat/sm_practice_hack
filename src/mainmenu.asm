ec191d33f284893b9c4ff1db63608b9b44ddcfc3
InsaneFirebat <InsaneFirebat@gmail.com>
2021-09-18 11:23:06 -0400

    Optimize Gamemode Shortcuts
    Same as "game_loop_extras" but skips checking the less common controller shortcuts when they aren't configured
    Flag is checked/toggled during RAM init and when configuring or clearing shortcuts

--- src/mainmenu.asm
+++ src/mainmenu.asm
@@ -2071,6 +2071,7 @@ ctrl_clear_shortcuts:
 action_clear_shortcuts:
 {
     TYA
+    STA !ram_gamemode_extras
     STA !sram_ctrl_save_state
     STA !sram_ctrl_load_state
     STA !sram_ctrl_load_last_preset
