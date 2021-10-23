51fbeb39442d236fbb1cd417be0b502e1aa4b813
InsaneFirebat <InsaneFirebat@gmail.com>
2021-10-22 20:21:46 -0400

    Patches and build scripts for v2.3.0

--- build.bat
+++ build.bat
@@ -8,25 +8,13 @@ echo Building saveless version
 copy *.sfc ..\build
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_2.2.X.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_2.3.X.ips
 
 echo Building savestate version
 copy *.sfc ..\build
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Savestates_2.2.X.ips
-
-echo Building saveless extras version
-copy *.sfc ..\build
-..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Experimental_InfoHUD_2.2.X.ips
-
-echo Building savestate extras version
-copy *.sfc ..\build
-..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Experimental_InfoHUD_Savestates_2.2.X.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Savestates_2.3.X.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
 cd ..
