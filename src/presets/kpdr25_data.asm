455882816414fe66a7d73864fd0441adfc7781b6
InsaneFirebat <InsaneFirebat@gmail.com>
2022-02-21 23:27:50 -0500

    Fix music with MB2/MB3/Zebes Escape presets
    by idlechild
    
    Co-Authored-By: idlechild <68176797+idlechild@users.noreply.github.com>

--- src/presets/kpdr25_data.asm
+++ src/presets/kpdr25_data.asm
@@ -1792,6 +1792,7 @@ preset_kpdr25_tourian_mother_brain_2:
     dw $078D, $AAC8  ; DDB
     dw $079B, $DD58  ; MDB
     dw $07F3, $0021  ; Music Bank
+    dw $07F5, $0000  ; Music Bank
     dw $090F, $79FF  ; Screen subpixel X position
     dw $0915, $0000  ; Screen Y position in pixels
     dw $0919, $0000  ; Layer 2 Y position
@@ -1805,7 +1806,6 @@ preset_kpdr25_tourian_mother_brain_2:
 
 preset_kpdr25_tourian_mother_brain_3:
     dw #preset_kpdr25_tourian_mother_brain_2 ; Tourian: Mother Brain 2
-    dw $07F3, $0048  ; Music Bank
     dw $09A6, $1009  ; Beams
     dw $09C2, $018F  ; Health
     dw $09C6, $0000  ; Missiles
@@ -1816,8 +1816,6 @@ preset_kpdr25_tourian_mother_brain_3:
 
 preset_kpdr25_tourian_zebes_escape:
     dw #preset_kpdr25_tourian_mother_brain_3 ; Tourian: Mother Brain 3
-    dw $07F3, $0024  ; Music Bank
-    dw $07F5, $0007  ; Music Track
     dw $09A6, $1009  ; Beams
     dw $0AF6, $0025  ; Samus X
     dw $0AFA, $00C3  ; Samus Y
@@ -1828,6 +1826,8 @@ preset_kpdr25_tourian_escape_room_3:
     dw #preset_kpdr25_tourian_zebes_escape ; Tourian: Zebes Escape
     dw $078D, $AAEC  ; DDB
     dw $079B, $DE7A  ; MDB
+    dw $07F3, $0024  ; Music Bank
+    dw $07F5, $0007  ; Music Track
     dw $090F, $C000  ; Screen subpixel X position
     dw $0915, $007C  ; Screen Y position in pixels
     dw $0919, $005D  ; Layer 2 Y position
