b88ff07a681e74cc9e8ace212e1c1d40d025abce
InsaneFirebat <InsaneFirebat@gmail.com>
2022-04-19 00:54:44 -0400

    Generate debugging symbols for dev builds

--- build_dev.bat
+++ build_dev.bat
@@ -4,6 +4,6 @@ echo Building InsaneFirebat's SM Practice Hack
 
 cd build
 echo Building and pre-patching saveless extras developer version
-cp sm_orig.sfc FastBuildDevTest.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 -DNEW_PHANTOON_RNG=1 -DFEATURE_DEV=1 main.asm ..\build\FastBuildDevTest.sfc && cd ..
+cp sm_orig.sfc FastBuildDevTest.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Custom_InfoHUD.sym -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 -DNEW_PHANTOON_RNG=1 -DFEATURE_DEV=1 main.asm ..\build\FastBuildDevTest.sfc && cd ..
 
 PAUSE
