573b1c3c64494966be5c768720627c85e097178f
InsaneFirebat <InsaneFirebat@gmail.com>
2022-05-22 03:21:29 -0400

    Update build scripts

--- build_fast.bat
+++ build_fast.bat
@@ -4,6 +4,6 @@ echo Building InsaneFirebat's SM Practice Hack
 
 cd build
 echo Building and pre-patching saveless extras version
-cp sm_orig.sfc FastBuildTest.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Firebat_InfoHUD.sym -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=1 -DFEATURE_DEV=1 main.asm ..\build\FastBuildTest.sfc && cd ..
+cp sm_orig.sfc aaaa_FastBuildTest.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Firebat_InfoHUD.sym -DFEATURE_TINYSTATES=1 -DFEATURE_EXTRAS=1 -DFEATURE_DEV=1 main.asm ..\build\aaaa_FastBuildTest.sfc && cd ..
 
 PAUSE
