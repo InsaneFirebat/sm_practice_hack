5d7162108b687244feefaa98ac127667353f4a13
InsaneFirebat <InsaneFirebat@gmail.com>
2022-05-22 12:32:29 -0400

    Fix build scripts

--- build.bat
+++ build.bat
@@ -18,8 +18,8 @@ python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Sav
 
 echo Building TinyStates version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_TinyStates_2.4.X.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
