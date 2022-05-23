c8191c1cb7f94e5ed8fc9488e8653bb28184a885
InsaneFirebat <InsaneFirebat@gmail.com>
2022-05-23 10:16:30 -0400

    Fix missing merge

--- buildall.bat
+++ buildall.bat
@@ -6,38 +6,38 @@ python create_dummies.py 00.sfc ff.sfc
 
 echo Building saveless version
 copy *.sfc ..\build
-..\tools\asar.exe --no-title-check -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe --no-title-check -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar.exe --no-title-check -DFEATURE_EXTRAS=0 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_2.4.X.ips
 
 echo Building SD2SNES version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Savestates_2.4.X.ips
 
 echo Building TinyStates version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_TinyStates_2.4.X.ips
 
 echo Building saveless extras version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD+_2.4.X.ips
 
 echo Building SD2SNES extras version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD+_Savestates_2.4.X.ips
 
 echo Building TinyStates extras version
 copy *.sfc ..\build
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\00.sfc
+..\tools\asar\asar.exe  --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_EXTRAS=1 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD+_TinyStates_2.4.X.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
