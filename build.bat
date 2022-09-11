d632419ddaf5de3574c12ed1fe29fd68ddb7f390
InsaneFirebat <InsaneFirebat@gmail.com>
2022-09-11 14:57:06 -0400

    Bump version to 2.5.0

--- build.bat
+++ build.bat
@@ -8,19 +8,19 @@ echo Building saveless version
 copy *.sfc ..\build
 ..\tools\asar\asar.exe --no-title-check ..\src\main.asm ..\build\00.sfc
 ..\tools\asar\asar.exe --no-title-check ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_2.4.X.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_2.5.X.ips
 
 echo Building savestate version
 copy *.sfc ..\build
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Savestates_2.4.X.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_Savestates_2.5.X.ips
 
 echo Building TinyStates version
 copy *.sfc ..\build
 ..\tools\asar\asar.exe  --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar\asar.exe  --no-title-check -DFEATURE_TINYSTATES=1 ..\src\main.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_TinyStates_2.4.X.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Custom_InfoHUD_TinyStates_2.5.X.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
 cd ..
--- build.sh
+++ build.sh
@@ -8,19 +8,19 @@ echo "Building saveless version"
 cp *.sfc ../build
 ../tools/asar/asar.exe --no-title-check ../src/main.asm ../build/00.sfc
 ../tools/asar/asar.exe --no-title-check ../src/main.asm ../build/ff.sfc
-python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_2.4.X.ips
+python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_2.5.X.ips
 
 echo "Building savestate version"
 cp *.sfc ../build
 ../tools/asar/asar.exe --no-title-check -DFEATURE_SD2SNES=1 ../src/main.asm ../build/00.sfc
 ../tools/asar/asar.exe --no-title-check -DFEATURE_SD2SNES=1 ../src/main.asm ../build/ff.sfc
-python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_Savestates_2.4.X.ips
+python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_Savestates_2.5.X.ips
 
 echo "Building TinyStates version"
 cp *.sfc ../build
 ../tools/asar/asar.exe --no-title-check -DFEATURE_TINYSTATES=1 ../src/main.asm ../build/00.sfc
 ../tools/asar/asar.exe --no-title-check -DFEATURE_TINYSTATES=1 ../src/main.asm ../build/ff.sfc
-python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_TinyStates_2.4.X.ips
+python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/Custom_InfoHUD_TinyStates_2.5.X.ips
 
 rm 00.sfc ff.sfc ../build/00.sfc ../build/ff.sfc
 cd ..
