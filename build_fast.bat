0cd124a8bb7d3dac0fdd5363b44a99d0865af8cb
InsaneFirebat <InsaneFirebat@gmail.com>
2022-09-24 11:05:34 -0400

    Default to savestate build

--- build_fast.bat
+++ build_fast.bat
@@ -3,7 +3,7 @@
 echo Building InsaneFirebat's SM Practice Hack
 
 cd build
-echo Building and pre-patching saveless extras version
-cp sm_orig.sfc aaaa_FastBuildTest.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Firebat_InfoHUD.sym -DFEATURE_TINYSTATES=1 -DFEATURE_EXTRAS=1 -DFEATURE_DEV=1 main.asm ..\build\aaaa_FastBuildTest.sfc && cd ..
+echo Building and pre-patching savestate extras version
+cp sm_orig.sfc Firebat_InfoHUD_Savestates_2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Firebat_InfoHUD.sym -DFEATURE_SD2SNES=1 -DFEATURE_TINYSTATES=0 -DFEATURE_EXTRAS=1 -DFEATURE_DEV=1 main.asm ..\build\Firebat_InfoHUD_Savestates_2.5.X.sfc && cd ..
 
 PAUSE
