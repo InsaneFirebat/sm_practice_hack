95d789a10d7aa4dfe524ed835610f827e9257402
InsaneFirebat <InsaneFirebat@gmail.com>
2022-09-26 10:06:29 -0400

    Fix build script again

--- build_fast.bat
+++ build_fast.bat
@@ -4,6 +4,6 @@ echo Building SM Practice Hack
 
 cd build
 echo Building and pre-patching tinystates version
-cp sm_orig.sfc Custom_InfoHUD_TinyStates.2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_TINYSTATES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 main.asm ..\build\Custom_InfoHUD_TinyStates.5.X.sfc && cd ..
+cp sm_orig.sfc Custom_InfoHUD_TinyStates_2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_TINYSTATES=1 -DFEATURE_PAL=0 -DFEATURE_EXTRAS=0 main.asm ..\build\Custom_InfoHUD_TinyStates_2.5.X.sfc && cd ..
 
 PAUSE
