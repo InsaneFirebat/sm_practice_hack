20e88f6e472e0a3387fe2525b348c39d834934de
InsaneFirebat <InsaneFirebat@gmail.com>
2022-11-14 10:56:24 -0500

    Add TinyStates build script

---
+++ build_tinystate.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Building InsaneFirebat's SM Practice Hack
+
+cd build
+echo Building and pre-patching savestate extras version
+cp sm_orig.sfc Firebat_InfoHUD_TinyStates_2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_TINYSTATES=1 -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 main.asm ..\build\Firebat_InfoHUD_TinyStates_2.5.X.sfc && cd ..
+
+PAUSE
