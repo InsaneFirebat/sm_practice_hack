4be7a7fe7525faf5a69ff94168a9cc7b3fc8249c
InsaneFirebat <InsaneFirebat@gmail.com>
2023-04-19 11:18:49 -0400

    Always Draw3 for lag frames

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 
 ## Changelog since v2.2.7
 
+* [2.5.3.6]  Changed lag counter to use a 3 digit draw every time
 * [2.5.3.5]  Preserve segment timer during loadstate freeze, instead of resetting it
 * [2.5.3.5]  Fixed custom HUD number graphics after loading a savestate
 * [2.5.3.4]  Added Samus I-Frames to common RAM Watch addresses
--- src/infohud.asm
+++ src/infohud.asm
@@ -712,7 +712,7 @@ endif
 
   .skipToLag
     LDA !sram_top_display_mode : CMP !TOP_HUD_VANILLA_INDEX : BEQ .vanilla_infohud_draw_lag_and_reserves
-    LDA !ram_last_room_lag : LDX #$0080 : JSR Draw4
+    LDA !ram_last_room_lag : LDX #$0082 : JSR Draw3
 
     ; Skip door lag and segment timer when shinetune enabled
     LDA !sram_display_mode : CMP !IH_MODE_SHINETUNE_INDEX : BEQ .end
@@ -908,7 +908,7 @@ endif
 
     ; Lag counter
     LDA !sram_top_display_mode : CMP !TOP_HUD_VANILLA_INDEX : BEQ .vanilla_infohud_draw_lag_and_reserves
-    LDA !ram_last_room_lag : LDX #$0080 : JSR Draw4
+    LDA !ram_last_room_lag : LDX #$0082 : JSR Draw3
 
     ; Skip door lag and segment timer when shinetune enabled
     LDA !sram_display_mode : CMP !IH_MODE_SHINETUNE_INDEX : BEQ .end
