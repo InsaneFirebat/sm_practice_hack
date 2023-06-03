1385f80d3508f7e02595b076be3f2bbdbbb5e64f
InsaneFirebat <InsaneFirebat@gmail.com>
2023-06-03 10:37:44 -0400

    Fix cap on BRB cycle timer
    It can't be over 512 or the ASL #6 will go negative

--- src/IFBmenu.asm
+++ src/IFBmenu.asm
@@ -141,7 +141,7 @@ ifb_brb_timer_clear:
     RTL
 
 ifb_brb_cycle_timer:
-    %cm_numfield_word("Cycle Timer (seconds)", !ram_cm_brb_set_cycle, 1, 600, 1, 10, .routine)
+    %cm_numfield_word("Cycle Timer (seconds)", !ram_cm_brb_set_cycle, 1, 512, .routine)
   .routine
     LDA !ram_cm_brb_set_cycle : BNE +
     LDA #$0009 ; default to ~10s
