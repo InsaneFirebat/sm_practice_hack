2b0f3c4d8c2ac817801aa0d972c0a0f050ae7e3f
InsaneFirebat <InsaneFirebat@gmail.com>
2023-07-13 21:17:04 -0400

    Merge to v2.5.6
    Merge pull request #170 from tewtal/next-update

--- build_dev.bat
+++ build_dev.bat
@@ -3,13 +3,18 @@
 echo Building SM NTSC Dev Z-Factor Practice Hack
 python names\create_names.py ..\src\roomnames.asm default_names.txt custom_names.txt
 cd resources
+python create_ram_symbols.py ..\src\defines.asm ..\src\symbols.asm
 python create_dummies.py 00.sfc ff.sfc
 
 echo Building SD2SNES Dev version
 copy *.sfc ..\build
 ..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Z-Factor_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Z-Factor_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Z-Factor_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc > ..\build\print_pc.log 2>&1
+type ..\build\print_pc.log
+python dos_to_unix.py ..\build\Z-Factor_Symbols.sym
+python sort_debug_symbols.py ..\build\Debugging_Symbols.sym ..\build\Debugging_Sorted.sym ..\build\Z-Factor_Combined.sym
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\Z-Factor_sd2snes.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
 cd ..
+PAUSE
