44685a354d5f84d44073f9284e912381af03006a
InsaneFirebat <InsaneFirebat@gmail.com>
2023-07-13 21:50:10 -0400

    Merge to v2.5.6
    Merge pull request #170 from tewtal/next-update

--- build_dev.bat
+++ build_dev.bat
@@ -3,12 +3,15 @@
 echo Building SM NTSC Dev Practice Hack
 python names\create_names.py ..\src\roomnames.asm default_names.txt custom_names.txt
 cd resources
+python create_ram_symbols.py ..\src\defines.asm ..\src\symbols.asm
 python create_dummies.py 00.sfc ff.sfc
 
 echo Building SD2SNES Dev version
 copy *.sfc ..\build
 ..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc > ..\build\print_pc.log 2>&1
+python dos_to_unix.py ..\build\VITALITY_Symbols.sym
+python sort_debug_symbols.py ..\build\VITALITY_Symbols.sym ..\build\VITALITY_Sorted.sym ..\build\VITALITY_Combined.sym
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\VITALITY_sd2snes.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
