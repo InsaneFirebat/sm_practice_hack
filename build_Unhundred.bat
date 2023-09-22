9e2d63a05e55ac2470e40110102d37bf0823cce3
InsaneFirebat <InsaneFirebat@gmail.com>
2023-09-22 08:21:33 -0400

    Fix crash in manage presets menu

--- build_Unhundred.bat
+++ build_Unhundred.bat
@@ -4,6 +4,6 @@ echo Building Unhundred Practice Hack
 
 cd build
 echo Building and pre-patching savestate version
-cp HACK\Unhundred.sfc aaaa_Unhundred.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 main.asm ..\build\aaaa_Unhundred.sfc && cd ..
+cp HACK\Unhundred.sfc aaaa_Unhundred.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 main.asm ..\build\aaaa_Unhundred.sfc && cd ..
 
 PAUSE
