9a4b2fb21d759ba3ac6dd6447c532a5ea5f2d96b
InsaneFirebat <InsaneFirebat@gmail.com>
2024-03-07 11:20:05 -0500

    Build scripts to 2.6.X

--- build_savestate.bat
+++ build_savestate.bat
@@ -4,6 +4,6 @@ echo Building Custom SM Practice Hack
 
 cd build
 echo Building and pre-patching savestate version
-cp sm_orig.sfc Custom_InfoHUD_Savestates_2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 main.asm ..\build\Custom_InfoHUD_Savestates_2.5.X.sfc && cd ..
+cp sm_orig.sfc Custom_InfoHUD_Savestates_2.6.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 main.asm ..\build\Custom_InfoHUD_Savestates_2.6.X.sfc && cd ..
 
 PAUSE
