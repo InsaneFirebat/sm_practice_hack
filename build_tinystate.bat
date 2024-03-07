1dbf5f5792241ffdaa3e538f22c51a5b606c4f79
InsaneFirebat <InsaneFirebat@gmail.com>
2024-03-07 11:20:05 -0500

    Build scripts to 2.6.X

--- build_tinystate.bat
+++ build_tinystate.bat
@@ -4,6 +4,6 @@ echo Building InsaneFirebat's SM Practice Hack
 
 cd build
 echo Building and pre-patching savestate extras version
-cp sm_orig.sfc Firebat_InfoHUD_TinyStates_2.5.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_TINYSTATES=1 -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 main.asm ..\build\Firebat_InfoHUD_TinyStates_2.5.X.sfc && cd ..
+cp sm_orig.sfc Firebat_InfoHUD_TinyStates_2.6.X.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_TINYSTATES=1 -DFEATURE_SD2SNES=0 -DFEATURE_EXTRAS=1 main.asm ..\build\Firebat_InfoHUD_TinyStates_2.6.X.sfc && cd ..
 
 PAUSE
