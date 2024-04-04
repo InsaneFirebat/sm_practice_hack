f26c9689d3cbc6e41d041a5271ca3c656f2a7b64
InsaneFirebat <InsaneFirebat@gmail.com>
2024-04-03 22:24:02 -0400

    Fix controller shortcuts being disabled after reset
    Fix missing call to init_wram_based_on_sram at boot

--- build_cliffhanger.bat
+++ build_cliffhanger.bat
@@ -4,6 +4,6 @@ echo Building Cliffhanger Practice Hacks
 
 cd build
 echo Building and pre-patching savestate versions
-cp HACK\Cliffhanger.sfc aaaa_Cliffhanger.sfc && cd ..\src && ..\tools\asar\asar.exe --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 main.asm ..\build\aaaa_Cliffhanger.sfc && cd ..
+cp HACK\Cliffhanger.sfc aaaa_Cliffhanger.sfc && cd ..\src && ..\tools\asar\asar.exe --symbols=wla --symbols-path=..\build\Cliffhangar_Symbols.sym --no-title-check -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 main.asm ..\build\aaaa_Cliffhanger.sfc && cd ..
 
 PAUSE
