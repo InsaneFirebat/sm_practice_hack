13f089e5c3f76b387f85d9dd55fd109a42c3cc51
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-03-17 22:56:07 -0700

    Fix quickboot SPC crash issue

--- src/menu.asm
+++ src/menu.asm
@@ -3440,6 +3440,7 @@ cm_spc_next_block:
 
     STA $2141
 
+    STY !cm_spc_data
     %a16()
     LDA.w #cm_spc_next_block_wait : STA !ram_quickboot_spc_state
 
@@ -3464,7 +3465,6 @@ cm_spc_next_block_wait:
     %a16()
     LDA !cm_spc_len : BEQ .eof
     LDA.w #cm_spc_transfer : STA !ram_quickboot_spc_state
-    STY !cm_spc_data
     RTS
 
   .eof
