a12246bc9b757414958420058b070e07e68fb902
idle <idlechild123@gmail.com>
2025-07-06 14:09:38 -0500

    Fix Samus 32x32 grid projectiles

--- src/spritefeat.asm
+++ src/spritefeat.asm
@@ -750,8 +750,8 @@ update_samusproj_sprite_hitbox:
     JMP .skipProjectile
 
   .check32x32
-    ; skip bombs ($0500), bomb explosions ($0501) and power bombs ($0300)
-    LDA !SAMUS_PROJ_PROPERTIES,X : BIT #$0701 : BNE .skipProjectile32x32
+    ; Only show beams, missiles, and super missiles
+    LDA !SAMUS_PROJ_PROPERTIES,X : AND #$0F00 : CMP #$0300 : BPL .skip32x32
 
     LDA !SAMUS_PROJ_X,X : CMP !LAYER1_X : BMI .skipProjectile32x32
     LDA !LAYER1_X : CLC : ADC #$0100 : CMP !SAMUS_PROJ_X,X : BMI .skipProjectile32x32
