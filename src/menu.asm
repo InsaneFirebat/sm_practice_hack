a47cb9640db657464bb9e2f5b4cf0fa76b60f03e
idle <idlechild123@gmail.com>
2026-01-24 23:13:46 -0600

    Fix execute_numfield_word LDA before JSL

--- src/menu.asm
+++ src/menu.asm
@@ -2821,7 +2821,7 @@ execute_numfield_word:
     PHK : PEA .end-1
 
     ; addr in A
-    LDA [!DP_Address] : LDX #$0000
+    LDA [!DP_DigitAddress] : LDX #$0000
     JML.w [!DP_JSLTarget]
 
   .singleDigitEditing
