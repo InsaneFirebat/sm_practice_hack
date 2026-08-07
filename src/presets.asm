5c2410732956c0346c0ee64250396037abf369bf
InsaneFirebat <InsaneFirebat@gmail.com>
2026-08-06 20:41:39 -0400

    Fix assembler errors

--- src/presets.asm
+++ src/presets.asm
@@ -892,6 +892,6 @@ incsrc presets/100map_data.asm ; 31FCh bytes
 incsrc presets/nghyper_data.asm ; E88h bytes
 incsrc presets/ngplasma_data.asm ; EA4h bytes
 incsrc presets/suitless_data.asm ; 3DF0h bytes
-warnpc START_FREESPACE_F0
+warnpc !START_FREESPACE_F0
 check bankcross on
 print pc, " crossbank preset_data.asm $E8..end"
