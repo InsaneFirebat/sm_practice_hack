7532e2c71c2a0ec285d66d4db0f24f956205f87a
InsaneFirebat <InsaneFirebat@gmail.com>
2026-08-10 20:49:00 -0400

    Submenu for Door Display Mode selection

--- src/infohud.asm
+++ src/infohud.asm
@@ -462,7 +462,7 @@ ih_before_room_transition:
     LDA !sram_display_mode : BNE .done
     LDA !sram_door_display_mode : BEQ .done
     ASL : TAX
-    JSR (.status_door_display_table,X)
+    JSR (status_door_display_table,X)
     ; Suppress Enemy HP display
     LDA !ENEMY_HP : STA !ram_enemy_hp
 
@@ -476,15 +476,24 @@ ih_before_room_transition:
     TYA : JSR Draw2
     BRA .displayMode
 
-  .status_door_display_table
-    dw #$0000 ; off/dummy
-    dw status_door_hspeed
-    dw status_door_vspeed
+status_door_display_table:    
+  .off
+    dw #$0000 ; off/dummy     
+  .hspeed
+    dw status_door_hspeed     
+  .vspeed
+    dw status_door_vspeed     
+  .chargetimer
     dw status_door_chargetimer
-    dw status_shinetimer
+  .shinetimer
+    dw status_shinetimer      
+  .dashcounter
     dw status_door_dashcounter
-    dw status_door_xpos
-    dw status_door_ypos
+  .xpos
+    dw status_door_xpos       
+  .ypos
+    dw status_door_ypos       
+  .end
 }
 
 ceres_start_timers:
